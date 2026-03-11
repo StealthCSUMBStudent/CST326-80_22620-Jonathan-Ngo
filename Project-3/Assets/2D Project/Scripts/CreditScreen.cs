35e10b7108619768f40cc9b437e7a549a677c0fb
Jonathan Ngo <143045531+StealthCSUMBStudent@users.noreply.github.com>
2026-03-11 00:04:28 -0700

    slight modifications to match with video upload.
    A fatal bug that caused the shooting enemy to instantly kill the player on start should be fixed now. the sounds sitll plays however.

--- Project-3/Assets/2D Project/Scripts/CreditScreen.cs
+++ Project-3/Assets/2D Project/Scripts/CreditScreen.cs
@@ -14,36 +14,7 @@ public class CreditScreen : MonoBehaviour
     {
         
     }
-    public void LoadGame()
-    {
-        StartCoroutine(_LoadGame());
-        //Debug.Log("hello anxiety my old friend");
 
-        IEnumerator _LoadGame()
-        {
-            AsyncOperation loadOperation = SceneManager.LoadSceneAsync("Schmup");
-            while (!loadOperation.isDone) yield return null;
-
-            //SceneManager.LoadScene("Schmup");
-
-            GameObject playerObj = GameObject.Find("Player");
-            Debug.Log(playerObj.name);
-        }
-    }
-
-    public void LoadCredits()
-    {
-        StartCoroutine(_LoadCredits());
-        //Debug.Log("hello anxiety my old friend");
-
-        IEnumerator _LoadCredits()
-        {
-            AsyncOperation loadOperation = SceneManager.LoadSceneAsync("CreditScreen");
-            while (!loadOperation.isDone) yield return null;
-
-            //SceneManager.LoadScene("CreditScene");
-        }
-    }
     IEnumerator LoadMenu(float delay)
     {
         yield return new WaitForSeconds(delay);
